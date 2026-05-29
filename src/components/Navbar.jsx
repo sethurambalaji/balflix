1f93a5fa62ef1d9ede0c906ba3ea319ebee42f07
sethurambalaji <s.sethurambalaji.s@gmail.com>
2026-05-29 08:33:21 +0530

    Nav Bar created

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <nav class="navbar navbar-dark bg-dark sticky-top">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Balflix</a>
+            </div>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar;
