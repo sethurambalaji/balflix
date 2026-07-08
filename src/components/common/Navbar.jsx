ad6f7b7fdb7f923cc8bba6b5b0aebefb28568b97
sethurambalaji <s.sethurambalaji.s@gmail.com>
2026-07-08 13:47:16 +0530

    search bar created

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import {LoginOutlined,ShoppingCartOutlined} from '@ant-design/icons';
-import { Space,Badge,Avatar } from 'antd';
+import {LoginOutlined,ShoppingCartOutlined,SearchOutlined } from '@ant-design/icons';
+import { Space,Badge,Tooltip,Button } from 'antd';
 import logo from '../../images/balflix_icon.png';
 
 const Navbar = () => {
@@ -20,26 +20,35 @@ const Navbar = () => {
                         }}
                     />
                 </a>
-                <div className="d-inline-flex justify-content-end align-items-right" 
-                    style={{paddingRight:'40px',marginLeft:'auto'}}>
-                    <a className="navbar-brand p-2" href="#" alt="Login">
-                        <Space>
-                            <LoginOutlined style={{ fontSize: '24px', color: '#FDF4AF' }} />
-                        </Space>
-                    </a>
+
+                <div>
+                    <div className="d-inline-flex justify-content-start" style={{padding:'1rem'}}>
+                        <form className="d-inline-flex" role="search">
+                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                            <button className="btn m-0 p-0" type="submit">
+                                <Tooltip title="search">
+                                    <Button type="primary" shape="circle" icon={<SearchOutlined />} />
+                                </Tooltip>
+                            </button>
+                        </form>
+                    </div>
+                    <div className="d-inline-flex justify-content-end align-items-right" 
+                        style={{paddingRight:'40px',marginLeft:'auto'}}>
+                        <a className="navbar-brand p-2" href="#" alt="Login">
+                            <Space>
+                                <LoginOutlined style={{ fontSize: '24px', color: '#FDF4AF' }} />
+                            </Space>
+                        </a>
                     
-                    <a className="navbar-brand p-2" href="#" alt="Shopping Cart">
-                       <Space>
-                            
-                            
-                                <Badge className="m-0 p-0" size="large" count={5} style={{position:'absolute',left:'15px'}}>
+                        <a className="navbar-brand p-2" href="#" alt="Shopping Cart">
+                            <Space>                           
+                                <Badge size="large" count={1} style={{position:'absolute',left:'15px'}}>
                                     <ShoppingCartOutlined style={{ fontSize: '24px', color: '#FDF4AF' }} />
                                 </Badge>
-                            
-                        </Space>
-                    </a>
+                            </Space>
+                        </a>
+                    </div>
                 </div>
-                
             </div>
         </nav>
     </div>
