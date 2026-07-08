6c3e151feff77e3b548ed53bdc076a493eb7dd99
sethurambalaji <s.sethurambalaji.s@gmail.com>
2026-07-08 13:18:13 +0530

    Background color changes, cart and badges created, icons aligned

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,23 +1,41 @@
 import React from 'react';
 import {LoginOutlined,ShoppingCartOutlined} from '@ant-design/icons';
-import { Space } from 'antd';
+import { Space,Badge,Avatar } from 'antd';
+import logo from '../../images/balflix_icon.png';
 
 const Navbar = () => {
   return(
-    <div >
-        <nav className="navbar navbar-dark sticky-top" style={{backgroundColor:'#720455',color:'#FDF4AF'}}>
-            <div className="container-fluid" style={{backgroundColor:'#720455',color:'#FDF4AF'}}>
-                <a className="navbar-brand" href="#">Balflix</a>
-                <div className="d-inline-flex justify-content-end align-items-right ">
-                    <a className="navbar-brand" href="#" alt="Login">
+    <div className='my-0'>
+        <nav className="navbar navbar-dark sticky-top py-0 " style={{backgroundColor:'#101D30',color:'#FDF4AF'}}>
+            <div className="container-fluid " style={{backgroundColor:'##101D30',color:'#FDF4AF'}}>
+                <a className="navbar-brand" href="#">
+                    <img 
+                        src={logo} 
+                        alt="custom-icon" 
+                        style={{ 
+                            width: '10em', 
+                            height: '4em', 
+                            objectFit: 'contain' ,
+                            paddingLeft:'0'
+                        }}
+                    />
+                </a>
+                <div className="d-inline-flex justify-content-end align-items-right" 
+                    style={{paddingRight:'40px',marginLeft:'auto'}}>
+                    <a className="navbar-brand p-2" href="#" alt="Login">
                         <Space>
                             <LoginOutlined style={{ fontSize: '24px', color: '#FDF4AF' }} />
                         </Space>
                     </a>
                     
-                    <a className="navbar-brand" href="#" alt="Shopping Cart">
+                    <a className="navbar-brand p-2" href="#" alt="Shopping Cart">
                        <Space>
-                            <ShoppingCartOutlined style={{ fontSize: '24px', color: '#FDF4AF' }} />
+                            
+                            
+                                <Badge className="m-0 p-0" size="large" count={5} style={{position:'absolute',left:'15px'}}>
+                                    <ShoppingCartOutlined style={{ fontSize: '24px', color: '#FDF4AF' }} />
+                                </Badge>
+                            
                         </Space>
                     </a>
                 </div>
